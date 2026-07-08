4736beda8ed3d52e12625eadfb012f15619ee600
kartik05i <jhakartik195@gmail.com>
2026-07-08 17:22:59 +0530

    database connention

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
+from app.database.database import engine
+from app.database.base import Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="ClipMind AI API",
     version="1.0.0",
     description="Backend API for ClipMind AI"
 )
 
+
 @app.get("/")
-def root():
+def home():
     return {
         "message": "Welcome to ClipMind AI 🚀"
     }
